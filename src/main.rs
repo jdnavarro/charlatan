55d7fec9bcca5872ae073a895a6a00e12cd7ae36
Danny Navarro <j@dannynavarro.net>
2020-05-08 18:59:33 +0100

    Pick bind IP address from environment

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
+use dotenv::dotenv;
+use std::env;
+use std::net::SocketAddr;
+
 use charlatan_server::{filters, models};
 
 #[tokio::main]
 async fn main() {
-    let pool = models::establish_pool();
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let pool = models::establish_pool(&database_url);
+
+    let bind_address: SocketAddr = env::var("BIND_ADDRESS")
+        .expect("BIND_ADDRESS is not set")
+        .parse()
+        .expect("BIND_ADDRESS is invalid");
 
     let routes = filters::api(pool);
 
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(routes).run(bind_address).await;
 }
--- src/models.rs
+++ src/models.rs
@@ -1,10 +1,7 @@
-use std::env;
-
 use diesel::{
     r2d2::{ConnectionManager, Pool, PooledConnection},
     sqlite::SqliteConnection,
 };
-use dotenv::dotenv;
 use serde::{Deserialize, Serialize};
 
 use super::schema::{episode, podcast};
@@ -12,11 +9,8 @@ use super::schema::{episode, podcast};
 pub type SqlitePool = Pool<ConnectionManager<SqliteConnection>>;
 pub type PooledSqliteConnection = PooledConnection<ConnectionManager<SqliteConnection>>;
 
-pub fn establish_pool() -> SqlitePool {
-    dotenv().ok();
-
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let manager = ConnectionManager::<SqliteConnection>::new(&database_url);
+pub fn establish_pool(database_url: &str) -> SqlitePool {
+    let manager = ConnectionManager::<SqliteConnection>::new(database_url);
 
     Pool::new(manager).expect(&format!("Error connecting to {}", database_url))
 }
