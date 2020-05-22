427ed9cd2038cfcf05a8063842bc8080467c759f
Danny Navarro <j@dannynavarro.net>
2020-05-22 13:56:12 +0100

    Reject instead of 400 for patch progress
    If more keywords are added to the PATCH method it should try multiple
    filters.

--- src/episode/handler.rs
+++ src/episode/handler.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::convert::Infallible;
 
 use sqlx::sqlite::SqlitePool;
 use warp::http::StatusCode;
@@ -10,21 +9,17 @@ pub(super) async fn position(
     p: SqlitePool,
     id: i32,
     hm: HashMap<String, i32>,
-) -> Result<impl warp::Reply, Infallible> {
-    match hm.get("position") {
-        Some(position) => match db::position(p, id, *position).await {
-            Ok(r) => Ok(warp::reply::with_status(
-                warp::reply::json(&r),
-                StatusCode::OK,
-            )),
-            Err(e) => Ok(warp::reply::with_status(
-                warp::reply::json(&format!("{}", e)),
-                StatusCode::INTERNAL_SERVER_ERROR,
-            )),
-        },
-        None => Ok(warp::reply::with_status(
-            warp::reply::json(&"Expecting position"),
-            StatusCode::BAD_REQUEST,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    // TODO: Custom error for expected progress key
+    let position = hm.get("position").ok_or(warp::reject::not_found())?;
+    match db::position(p, id, *position).await {
+        Ok(r) => Ok(warp::reply::with_status(
+            warp::reply::json(&r),
+            StatusCode::OK,
+        )),
+        Err(e) => Ok(warp::reply::with_status(
+            warp::reply::json(&format!("{}", e)),
+            StatusCode::INTERNAL_SERVER_ERROR,
         )),
     }
 }
