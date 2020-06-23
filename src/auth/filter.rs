454cd5163fe8116be372e68eac6a322b9254042a
Danny Navarro <j@dannynavarro.net>
2020-06-23 16:26:52 +0100

    Login endpoint
    See #5.

--- src/auth/filter.rs
+++ src/auth/filter.rs
@@ -7,9 +7,25 @@ use crate::{json_body, with_pool};
 pub fn api(
     pool: SqlitePool,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    register(pool.clone()).or(login(pool))
+}
+
+fn register(
+    pool: SqlitePool,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     with_pool(pool)
         .and(warp::path!("register"))
         .and(warp::post())
         .and(json_body())
         .and_then(handler::register)
+}
+
+fn login(
+    pool: SqlitePool,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    with_pool(pool)
+        .and(warp::path!("login"))
+        .and(warp::post())
+        .and(json_body())
+        .and_then(handler::login)
 }
--- src/auth/handler.rs
+++ src/auth/handler.rs
@@ -33,8 +33,35 @@ pub(super) async fn register(
     }
 }
 
+pub(super) async fn login(
+    p: SqlitePool,
+    credentials: User,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    match db::get(p, &credentials.name).await {
+        Err(auth::Error::NotFound) => {
+            log::warn!(
+                "Trying to login with unexisting user: {}",
+                &credentials.name
+            );
+            Ok(StatusCode::BAD_REQUEST)
+        }
+        Err(_) => Ok(StatusCode::INTERNAL_SERVER_ERROR),
+        Ok(user) => {
+            if verify(&user.password, credentials.password.as_bytes()) {
+                Ok(StatusCode::OK)
+            } else {
+                Ok(StatusCode::UNAUTHORIZED)
+            }
+        }
+    }
+}
+
 fn hash(password: &[u8]) -> String {
     let salt = rand::thread_rng().gen::<[u8; 32]>();
     let config = argon2::Config::default();
     argon2::hash_encoded(password, &salt, &config).unwrap()
+}
+
+pub fn verify(hash: &str, password: &[u8]) -> bool {
+    argon2::verify_encoded(hash, password).unwrap_or(false)
 }
